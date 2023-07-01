79f579a1a14451a24b09d85e0b00e4781d10a94e
abinash <stickatstack@outlook.com>
2023-07-01 15:40:17 +0545

    fix modules error

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,9 +1,7 @@
 const Profile = require("../models/Profile");
 const User = require("../models/User");
 const { uploadImageToCloudinary } = require("../utils/imageUploader");
-
-
-
+// Method for updating a profile
 exports.updateProfile = async (req, res) => {
 	try {
 		const { dateOfBirth = "", about = "", contactNumber } = req.body;
@@ -37,7 +35,14 @@ exports.updateProfile = async (req, res) => {
 
 exports.deleteAccount = async (req, res) => {
 	try {
+		// TODO: Find More on Job Schedule
+		// const job = schedule.scheduleJob("10 * * * * *", function () {
+		// 	console.log("The answer to life, the universe, and everything!");
+		// });
+		// console.log(job);
+		console.log("Printing ID: ", req.user.id);
 		const id = req.user.id;
+		
 		const user = await User.findById({ _id: id });
 		if (!user) {
 			return res.status(404).json({
@@ -45,8 +50,11 @@ exports.deleteAccount = async (req, res) => {
 				message: "User not found",
 			});
 		}
-		await Profile.findByIdAndDelete({ _id: user.userDetails });
-		await user.findByIdAndDelete({ _id: id });
+		// Delete Assosiated Profile with the User
+		await Profile.findByIdAndDelete({ _id: user.additionalDetails });
+		// TODO: Unenroll User From All the Enrolled Courses
+		// Now Delete User
+		await User.findByIdAndDelete({ _id: id });
 		res.status(200).json({
 			success: true,
 			message: "User deleted successfully",
@@ -79,4 +87,57 @@ exports.getAllUserDetails = async (req, res) => {
 	}
 };
 
-//updateDisplayPicture
+exports.updateDisplayPicture = async (req, res) => {
+    try {
+      const displayPicture = req.files.displayPicture
+      const userId = req.user.id
+      const image = await uploadImageToCloudinary(
+        displayPicture,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
+      console.log(image)
+      const updatedProfile = await User.findByIdAndUpdate(
+        { _id: userId },
+        { image: image.secure_url },
+        { new: true }
+      )
+      res.send({
+        success: true,
+        message: `Image Updated successfully`,
+        data: updatedProfile,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
+  
+exports.getEnrolledCourses = async (req, res) => {
+    try {
+      const userId = req.user.id
+      const userDetails = await User.findOne({
+        _id: userId,
+      })
+        .populate("courses")
+        .exec()
+      if (!userDetails) {
+        return res.status(400).json({
+          success: false,
+          message: `Could not find user with id: ${userDetails}`,
+        })
+      }
+      return res.status(200).json({
+        success: true,
+        data: userDetails.courses,
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+      })
+    }
+};
